12617a4ab6048333bb547cf14ed1807dff052bf1
Vladimir Teplyakov <vteplakov@gmail.com>
2026-02-07 18:23:58 +0300

    Добавлен реквестер, переписаны тесты

--- conftest.py
+++ conftest.py
@@ -12,8 +12,9 @@ from requests import session
 from constants import BASE_URL, HEADERS, REGISTER_ENDPONT, LOGIN_ENDPOINT
 import pytest
 from utils.data_generator import DataGenerator
+from custom_requester.custom_requester import CustomRequester
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def test_user():
     """
     Генерация случайного пользователя для тестов
@@ -30,28 +31,27 @@ def test_user():
         "roles": ["USER"]
     }
 
-    @pytest.fixture(scope="session")
-    def auth_session(test_user):
-        # Регистрируем нового пользователя
-        register_url = f"{BASE_URL}{REGISTER_ENDPONT}"
-        response = requests.post(register_url, json=test_user, headers=HEADERS)
-        assert response.status_code == 201, "Ошибка регистрации полььзователя"
+@pytest.fixture()
+def registered_user(requester, test_user):
+    """
+    Фикстура для регистрации и получения данных зарегистрированного пользователя.
+    """
+    response = requester.send_request(
+        method = "POST",
+        endpoint = REGISTER_ENDPONT,
+        data = test_user,
+        expected_status = 201
+    )
+    response_data = response.json()
+    registered_user = test_user.copy()
+    registered_user["id"] = response_data["id"]
+    return registered_user
 
-        #Логинимся для получения токена
-        login_url = f"{BASE_URL}{LOGIN_ENDPOINT}"
-        login_data = {
-            "email":test_user["email"],
-            "password": test_user["password"]
-        }
-        response = requests.post(login_url, json=login_data, headers=HEADERS)
-        assert response.status_code == 200, "Ошибка авторизации"
-
-        #Получаем токен и создаем сессию
-        token = response.json().get("accessToken")
-        assert token is not None, "Токен доступа отсутствует в ответе"
-
-        session = requests.Session()
-        session.headers.update(HEADERS)
-        session.headers.update({"Autorization": f"Bearer {token}"})
-        return session
+@pytest.fixture(scope="session")
+def requester():
+    """
+    Фикстура для создания экземпляра CustomRequester.
+    """
+    session = requests.Session()
+    return CustomRequester(session=session, base_url=BASE_URL)
 
